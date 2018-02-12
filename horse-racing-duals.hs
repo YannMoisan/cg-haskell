c57e7af462bdb91d47e4081472d516ecebb2b2d2
Yann Moisan <yamo93@gmail.com>
2018-02-12 13:26:19 +0100

    refactor : zipWith and minimum

--- horse-racing-duals.hs
+++ horse-racing-duals.hs
@@ -2,7 +2,6 @@ import System.IO
 import Control.Monad
 import Data.List
 
--- https://www.codingame.com/ide/puzzle/horse-racing-duals
 main :: IO ()
 main = do
     hSetBuffering stdout NoBuffering -- DO NOT REMOVE
@@ -18,10 +17,8 @@ main = do
         let pi = read input_line :: Int
         return (pi)
 
-    strengths >>= print.diffBetweenClosest
+    strengths >>= print.diffBetweenClosest.sort
     return ()
 
 diffBetweenClosest :: [Int] -> Int
-diffBetweenClosest xs = foldr (\(a, b) acc -> min (b - a) acc) 10000 (windowed xs)
-    where
-      windowed xs = zip (sort xs) (tail (sort xs))
+diffBetweenClosest xs = minimum (zipWith (-) (tail xs) xs)
